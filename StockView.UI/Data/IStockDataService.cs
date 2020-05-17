dba540420497e7705e2e16ce6981c69231212960
Genora51 <Geno.racklinasher@gmail.com>
2020-05-17 19:28:47 +0100

    Set up data service

---
+++ StockView.UI/Data/IStockDataService.cs
@@ -0,0 +1,10 @@
+﻿using StockView.Model;
+using System.Collections.Generic;
+
+namespace StockView.UI.Data
+{
+    public interface IStockDataService
+    {
+        IEnumerable<Stock> GetAll();
+    }
+}
