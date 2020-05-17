400b6ed2e8e5c36b4122fc28f2e8cde7dfd7c4cc
Genora51 <Geno.racklinasher@gmail.com>
2020-05-17 18:40:44 +0100

    Add project files.

---
+++ StockView.Model/Stock.cs
@@ -0,0 +1,13 @@
+﻿namespace StockView.Model
+{
+    public class Stock
+    {
+        public int Id { get; set; }
+
+        public string Symbol { get; set; }
+
+        public string CompanyName { get; set; }
+
+        public string Industry { get; set; }
+    }
+}
