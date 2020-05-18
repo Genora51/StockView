5bc556c18f81e2fafd5f653db2dc3f53434cbe2a
Genora51 <Geno.racklinasher@gmail.com>
2020-05-18 20:21:44 +0100

    Subscribe to OpenStockDetailViewEvent

--- StockView.UI/ViewModel/StockDetailViewModel.cs
+++ StockView.UI/ViewModel/StockDetailViewModel.cs
@@ -1,9 +1,8 @@
-﻿using StockView.Model;
+﻿using Prism.Events;
+using StockView.Model;
 using StockView.UI.Data;
+using StockView.UI.Event;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace StockView.UI.ViewModel
@@ -11,10 +10,20 @@ namespace StockView.UI.ViewModel
     public class StockDetailViewModel : ViewModelBase, IStockDetailViewModel
     {
         private IStockDataService _dataService;
+        private IEventAggregator _eventAggregator;
 
-        public StockDetailViewModel(IStockDataService dataService)
+        public StockDetailViewModel(IStockDataService dataService,
+            IEventAggregator eventAggregator)
         {
             _dataService = dataService;
+            _eventAggregator = eventAggregator;
+            _eventAggregator.GetEvent<OpenStockDetailViewEvent>()
+                .Subscribe(OnOpenStockDetailView);
+        }
+
+        private async void OnOpenStockDetailView(int stockId)
+        {
+            await LoadAsync(stockId);
         }
 
         public async Task LoadAsync(int stockId)
