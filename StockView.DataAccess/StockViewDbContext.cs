aeb080891fbc0b60bf9f990a7874a134c06293fc
Genora51 <Geno.racklinasher@gmail.com>
2020-05-18 13:13:24 +0100

    Create DbContext subclass

---
+++ StockView.DataAccess/StockViewDbContext.cs
@@ -0,0 +1,14 @@
+﻿using StockView.Model;
+using System.Data.Entity;
+
+namespace StockView.DataAccess
+{
+    public class StockViewDbContext : DbContext
+    {
+        public StockViewDbContext() : base("StockViewDb")
+        {
+
+        }
+        public DbSet<Stock> Stocks { get; set; }
+    }
+}
