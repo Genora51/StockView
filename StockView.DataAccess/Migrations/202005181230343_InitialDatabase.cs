9c7d67bbdfd51895acbc902a493c74be32d7031f
Genora51 <Geno.racklinasher@gmail.com>
2020-05-18 13:32:42 +0100

    Add code first migration

---
+++ StockView.DataAccess/Migrations/202005181230343_InitialDatabase.cs
@@ -0,0 +1,28 @@
+﻿namespace StockView.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialDatabase : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Stocks",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Symbol = c.String(),
+                        CompanyName = c.String(),
+                        Industry = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Stocks");
+        }
+    }
+}
---
+++ StockView.DataAccess/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace StockView.DataAccess.Migrations
+{
+    using StockView.Model;
+    using System.Data.Entity.Migrations;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<StockView.DataAccess.StockViewDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(StockView.DataAccess.StockViewDbContext context)
+        {
+            context.Stocks.AddOrUpdate(
+                s => s.Symbol,
+                new Stock { CompanyName = "Alphabet Inc.", Symbol = "GOOGL", Industry = "Technology" },
+                new Stock { CompanyName = "Home Depot", Symbol = "HD", Industry = "Consumer Cyclical" },
+                new Stock { CompanyName = "Johnson & Johnson", Symbol = "JNJ", Industry = "Healthcare" }
+                );
+        }
+    }
+}
