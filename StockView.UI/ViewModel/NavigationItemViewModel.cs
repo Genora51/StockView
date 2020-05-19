c7b1c6e0bec4c52cdf54b09fdf8a449329b96b72
Genora51 <Geno.racklinasher@gmail.com>
2020-05-19 14:25:11 +0100

    Remove navigation selection

--- StockView.UI/ViewModel/NavigationItemViewModel.cs
+++ StockView.UI/ViewModel/NavigationItemViewModel.cs
@@ -1,13 +1,28 @@
-﻿namespace StockView.UI.ViewModel
+﻿using Prism.Commands;
+using Prism.Events;
+using StockView.UI.Event;
+using System.Windows.Input;
+
+namespace StockView.UI.ViewModel
 {
 	public class NavigationItemViewModel : ViewModelBase
     {
 		private string _displayMember;
+		private IEventAggregator _eventAggregator;
 
-		public NavigationItemViewModel(int id, string displayMember)
+		public NavigationItemViewModel(int id, string displayMember,
+			IEventAggregator eventAggregator)
 		{
+			_eventAggregator = eventAggregator;
 			Id = id;
 			DisplayMember = displayMember;
+			OpenStockDetailViewCommand = new DelegateCommand(OnOpenStockDetailView);
+		}
+
+		private void OnOpenStockDetailView()
+		{
+			_eventAggregator.GetEvent<OpenStockDetailViewEvent>()
+						.Publish(Id);
 		}
 
 		public int Id { get; }
@@ -20,6 +35,8 @@
 				OnPropertyChanged();
 			}
 		}
+
+		public ICommand OpenStockDetailViewCommand { get; }
 
 	}
 }
