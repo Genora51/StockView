a558ea3dc485cd5884f0584fd58cd92658fc99c5
Genora51 <Geno.racklinasher@gmail.com>
2020-05-19 10:59:18 +0100

    Process unhandled exceptions

--- StockView.UI/App.xaml.cs
+++ StockView.UI/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Autofac;
 using StockView.UI.Startup;
+using System;
 using System.Windows;
 
 namespace StockView.UI
@@ -16,6 +17,15 @@ namespace StockView.UI
 
             var mainWindow = container.Resolve<MainWindow>();
             mainWindow.Show();
+        }
+
+        private void Application_DispatcherUnhandledException(object sender,
+            System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("Unexpected error occurred. Please inform the administrator."
+                + Environment.NewLine + e.Exception.Message, "Unexpected error");
+
+            e.Handled = true;
         }
     }
 }
