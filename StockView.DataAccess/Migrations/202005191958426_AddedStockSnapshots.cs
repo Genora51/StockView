8c3daf2243791cf8fc59a4496123e2a2c574fdd9
Genora51 <Geno.racklinasher@gmail.com>
2020-05-19 21:10:43 +0100

    Create StockSnapshot entity

---
+++ StockView.DataAccess/Migrations/202005191958426_AddedStockSnapshots.cs
@@ -0,0 +1,31 @@
+﻿namespace StockView.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedStockSnapshots : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.StockSnapshots",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Date = c.DateTime(nullable: false, storeType: "date"),
+                        StockId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Stocks", t => t.StockId, cascadeDelete: true)
+                .Index(t => t.StockId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.StockSnapshots", "StockId", "dbo.Stocks");
+            DropIndex("dbo.StockSnapshots", new[] { "StockId" });
+            DropTable("dbo.StockSnapshots");
+        }
+    }
+}
---
+++ StockView.Model/StockSnapshot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace StockView.Model
+{
+    public class StockSnapshot
+    {
+        public int Id { get; set; }
+
+        [DataType(DataType.Date)]
+        [Required]
+        public DateTime Date { get; set; }
+
+        public int StockId { get; set; }
+
+        public Stock Stock { get; set; }
+    }
+}
