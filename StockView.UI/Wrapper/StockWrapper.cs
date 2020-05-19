9c8d929480b68d2d061f4743c8cdced939c84b86
Genora51 <Geno.racklinasher@gmail.com>
2020-05-19 12:23:38 +0100

    Create NotifyDataErrorInfoBase

--- StockView.UI/Wrapper/StockWrapper.cs
+++ StockView.UI/Wrapper/StockWrapper.cs
@@ -1,14 +1,9 @@
 ﻿using StockView.Model;
-using StockView.UI.ViewModel;
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
 
 namespace StockView.UI.Wrapper
 {
-    public class StockWrapper : ViewModelBase, INotifyDataErrorInfo
+    public class StockWrapper : NotifyDataErrorInfoBase
     {
         public StockWrapper(Stock model)
         {
@@ -61,47 +56,6 @@ namespace StockView.UI.Wrapper
             {
                 Model.Industry = value;
                 OnPropertyChanged();
-            }
-        }
-
-        private Dictionary<string, List<string>> _errorsByPropertyName
-            = new Dictionary<string, List<string>>();
-
-        public bool HasErrors => _errorsByPropertyName.Any();
-
-        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
-
-        public IEnumerable GetErrors(string propertyName)
-        {
-            return _errorsByPropertyName.ContainsKey(propertyName)
-                ? _errorsByPropertyName[propertyName]
-                : null;
-        }
-
-        private void OnErrorsChanged(string propertyName)
-        {
-            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
-        }
-
-        private void AddError(string propertyName, string error)
-        {
-            if (!_errorsByPropertyName.ContainsKey(propertyName))
-            {
-                _errorsByPropertyName[propertyName] = new List<string>();
-            }
-            if (!_errorsByPropertyName[propertyName].Contains(error))
-            {
-                _errorsByPropertyName[propertyName].Add(error);
-                OnErrorsChanged(propertyName);
-            }
-        }
-
-        private void ClearErrors(string propertyName)
-        {
-            if (_errorsByPropertyName.ContainsKey(propertyName))
-            {
-                _errorsByPropertyName.Remove(propertyName);
-                OnErrorsChanged(propertyName);
             }
         }
     }
