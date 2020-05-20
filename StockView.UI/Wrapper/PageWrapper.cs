cb3b225fead878cce2ef1304d5b0f0db102a5f3b
Genora51 <Geno.racklinasher@gmail.com>
2020-05-20 17:57:03 +0100

    Wrap Page entity

---
+++ StockView.UI/Wrapper/PageWrapper.cs
@@ -0,0 +1,19 @@
+﻿using StockView.Model;
+
+namespace StockView.UI.Wrapper
+{
+    public class PageWrapper : ModelWrapper<Page>
+    {
+        public PageWrapper(Page model) : base(model)
+        {
+        }
+
+        public int Id { get { return Model.Id; } }
+
+        public string Title
+        {
+            get { return GetValue<string>(); }
+            set { SetValue(value); }
+        }
+    }
+}
