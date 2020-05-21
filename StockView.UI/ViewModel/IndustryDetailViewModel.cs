5204f6b8cd9d09dcdb03575249260188d3cf1c0c
Genora51 <Geno.racklinasher@gmail.com>
2020-05-21 17:52:49 +0100

    Implement IndustryDetailViewModel

--- StockView.UI/ViewModel/IndustryDetailViewModel.cs
+++ StockView.UI/ViewModel/IndustryDetailViewModel.cs
@@ -1,23 +1,62 @@
-﻿using Prism.Events;
+﻿using Prism.Commands;
+using Prism.Events;
+using StockView.UI.Data.Repositories;
 using StockView.UI.View.Services;
+using StockView.UI.Wrapper;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace StockView.UI.ViewModel
 {
     public class IndustryDetailViewModel : DetailViewModelBase
     {
+        private IIndustryRepository _industryRepository;
+
         public IndustryDetailViewModel(IEventAggregator eventAggregator,
-            IMessageDialogService messageDialogService)
+            IMessageDialogService messageDialogService,
+            IIndustryRepository industryRepository)
             : base(eventAggregator, messageDialogService)
         {
+            _industryRepository = industryRepository;
             Title = "Industries";
+            Industries = new ObservableCollection<IndustryWrapper>();
         }
 
-        public override Task LoadAsync(int id)
+        public ObservableCollection<IndustryWrapper> Industries { get; }
+
+        public async override Task LoadAsync(int id)
         {
             // TODO: Load data here
             Id = id;
-            return Task.Delay(0);
+            foreach (var wrapper in Industries)
+            {
+                wrapper.PropertyChanged -= Wrapper_PropertyChanged;
+            }
+
+            Industries.Clear();
+
+            var industries = await _industryRepository.GetAllAsync();
+            foreach (var model in industries)
+            {
+                var wrapper = new IndustryWrapper(model);
+                wrapper.PropertyChanged += Wrapper_PropertyChanged;
+                Industries.Add(wrapper);
+            }
+        }
+
+        private void Wrapper_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (!HasChanges)
+            {
+                HasChanges = _industryRepository.HasChanges();
+            }
+            if (e.PropertyName == nameof(IndustryWrapper.HasErrors))
+            {
+                ((DelegateCommand)SaveCommand).RaiseCanExecuteChanged();
+            }
         }
 
         protected override void OnDeleteExecute()
@@ -27,12 +66,13 @@ namespace StockView.UI.ViewModel
 
         protected override bool OnSaveCanExecute()
         {
-            throw new System.NotImplementedException();
+            return HasChanges && Industries.All(i => !i.HasErrors);
         }
 
-        protected override void OnSaveExecute()
+        protected async override void OnSaveExecute()
         {
-            throw new System.NotImplementedException();
+            await _industryRepository.SaveAsync();
+            HasChanges = _industryRepository.HasChanges();
         }
     }
 }
---
+++ StockView.UI/Wrapper/IndustryWrapper.cs
@@ -0,0 +1,19 @@
+﻿using StockView.Model;
+
+namespace StockView.UI.Wrapper
+{
+    public class IndustryWrapper : ModelWrapper<Industry>
+    {
+        public IndustryWrapper(Industry model) : base(model)
+        {
+        }
+
+        public int Id { get { return Model.Id; } }
+
+        public string Name
+        {
+            get { return GetValue<string>(); }
+            set { SetValue(value); }
+        }
+    }
+}
