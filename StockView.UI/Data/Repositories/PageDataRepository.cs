b4e9af1e821f625629329f2b320861f5ef766bf8
Genora51 <Geno.racklinasher@gmail.com>
2020-05-25 15:38:00 +0100

    Create PageDataRepository

---
+++ StockView.UI/Data/Repositories/PageDataRepository.cs
@@ -0,0 +1,27 @@
+﻿using StockView.DataAccess;
+using StockView.Model;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace StockView.UI.Data.Repositories
+{
+    public class PageDataRepository : GenericRepository<Page, StockViewDbContext>,
+        IPageDataRepository
+    {
+        public PageDataRepository(StockViewDbContext context) : base(context)
+        {
+        }
+
+        public async override Task<Page> GetByIdAsync(int id)
+        {
+            var page = await Context.Pages
+                .SingleAsync(p => p.Id == id);
+            var entry = Context.Entry(page);
+            await entry.Collection(p => p.Stocks)
+                .Query()
+                .Include(s => s.Snapshots)
+                .LoadAsync();
+            return page;
+        }
+    }
+}
