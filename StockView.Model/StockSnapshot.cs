34bd26d9a760814471d741b97f7c9a69d8632a4e
Genora51 <Geno.racklinasher@gmail.com>
2020-05-25 13:32:00 +0100

    Unique date on snapshots

--- StockView.Model/StockSnapshot.cs
+++ StockView.Model/StockSnapshot.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace StockView.Model
 {
@@ -9,10 +10,12 @@ namespace StockView.Model
 
         [DataType(DataType.Date)]
         [Required]
+        [Index("IX_StockDate", 2, IsUnique = true)]
         public DateTime Date { get; set; }
 
         public float Value { get; set; }
 
+        [Index("IX_StockDate", 1, IsUnique = true)]
         public int StockId { get; set; }
 
         public Stock Stock { get; set; }
