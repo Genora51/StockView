79e302461f99f55be4f33d3ce443c9502b8864f8
Genora51 <Geno.racklinasher@gmail.com>
2020-07-15 12:26:28 +0100

    Implement IStockWebServiceClient

--- StockView.Fetch/Client/IStockWebServiceClient.cs
+++ StockView.Fetch/Client/IStockWebServiceClient.cs
@@ -1,9 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 
 namespace StockView.Fetch.Client
 {
     public interface IStockWebServiceClient
     {
-        // Task<decimal> 
+        Task<decimal?> GetValueAsync(string symbol, DateTime date);
+        Task<DateTime?> GetExDividendsAsync(string symbol);
     }
 }
--- StockView.Fetch/Client/StockWebServiceClient.cs
+++ StockView.Fetch/Client/StockWebServiceClient.cs
@@ -1,6 +1,67 @@
-﻿namespace StockView.Fetch.Client
+﻿using Flurl;
+using Flurl.Http;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading.Tasks;
+
+namespace StockView.Fetch.Client
 {
     public class StockWebServiceClient : IStockWebServiceClient
     {
+        private const string APIUrl = @"https://cloud.iexapis.com/stable";
+        private string _key;
+
+        public StockWebServiceClient(string key)
+        {
+            _key = key;
+        }
+
+        public async Task<DateTime?> GetExDividendsAsync(string symbol)
+        {
+            var url = APIUrl
+                .AppendPathSegments("stock", symbol, "dividends", "ytd")
+                .SetQueryParam("token", _key);
+            IList<dynamic> result;
+            try
+            {
+                result = await url.GetJsonListAsync();
+            } catch (FlurlHttpException)
+            {
+                return null;
+            }
+            if (result.Count > 0)
+            {
+                if (DateTime.TryParseExact(result[0].exDate, "yyyy-MM-dd",
+                        null, DateTimeStyles.None, out DateTime parsedDate))
+                    return parsedDate;
+                else return null;
+            }
+            else return null;
+        }
+
+        public async Task<decimal?> GetValueAsync(string symbol, DateTime date)
+        {
+            var url = APIUrl
+                .AppendPathSegments("stock", symbol, "chart", "date", $"{date:yyyyMMdd}")
+                .SetQueryParams(new
+                {
+                    token = _key,
+                    chartByDay = "true"
+                });
+            try
+            {
+                var result = await url.GetJsonListAsync();
+                if (result.Count > 0)
+                {
+                    return (decimal)result[0].uClose;
+                }
+                else return null;
+            } catch (FlurlHttpException)
+            {
+                return null;
+            }
+        }
     }
 }
