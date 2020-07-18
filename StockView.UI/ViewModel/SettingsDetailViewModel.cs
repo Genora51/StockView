9af3088e5b8ab78ba292e1b25eafec7844f8fb3e
Genora51 <Geno.racklinasher@gmail.com>
2020-07-18 16:05:03 +0100

    Create SettingsDetailViewModel

---
+++ StockView.UI/ViewModel/SettingsDetailViewModel.cs
@@ -0,0 +1,59 @@
+﻿using Prism.Commands;
+using Prism.Events;
+using StockView.UI.View.Services;
+using System;
+using System.Threading.Tasks;
+
+namespace StockView.UI.ViewModel
+{
+    public class SettingsDetailViewModel : DetailViewModelBase
+    {
+        // TODO: use a propper wrapper object [enable validation etc]
+        private string _apiKey;
+        private string originalApiKey;
+
+        public SettingsDetailViewModel(IEventAggregator eventAggregator,
+            IMessageDialogService messageDialogService)
+            : base(eventAggregator, messageDialogService)
+        { }
+
+        public string ApiKey
+        {
+            get { return _apiKey; }
+            set {
+                _apiKey = value;
+                if (!HasChanges)
+                {
+                    HasChanges = originalApiKey != _apiKey;
+                }
+                OnPropertyChanged();
+            }
+        }
+
+        public override Task LoadAsync(int id)
+        {
+            Id = id;
+            originalApiKey = Properties.Settings.Default["api_key"] as string;
+            ApiKey = originalApiKey;
+            return Task.CompletedTask;
+        }
+
+        protected override void OnDeleteExecute()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool OnSaveCanExecute()
+        {
+            return HasChanges;
+        }
+
+        protected override void OnSaveExecute()
+        {
+            Properties.Settings.Default["api_key"] = ApiKey;
+            originalApiKey = ApiKey;
+            HasChanges = false;
+            ((DelegateCommand)SaveCommand).RaiseCanExecuteChanged();
+        }
+    }
+}
