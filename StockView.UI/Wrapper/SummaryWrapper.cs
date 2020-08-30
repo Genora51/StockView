2491b53bdb7a7c2b816cc40534e41452cd6d27f7
Geno Racklin Asher <geno.racklinasher@gmail.com>
2020-08-30 19:06:17 +0100

    Add Summary wrapper

---
+++ StockView.UI/Wrapper/SummaryWrapper.cs
@@ -0,0 +1,25 @@
+﻿using StockView.Model;
+
+namespace StockView.UI.Wrapper
+{
+    public class SummaryWrapper : ModelWrapper<Summary>
+    {
+        public SummaryWrapper(Summary model) : base(model)
+        {
+        }
+
+        public int Id { get { return Model.Id; } }
+
+        public string Name
+        {
+            get { return GetValue<string>(); }
+            set { SetValue(value); }
+        }
+
+        public string Code
+        {
+            get { return GetValue<string>(); }
+            set { SetValue(value); }
+        }
+    }
+}
