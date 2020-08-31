2a5f4600cae88dc303678f69bc519e7f0b22790c
Geno Racklin Asher <geno.racklinasher@gmail.com>
2020-08-31 16:44:35 +0100

    Update formatting

--- StockView.UI/View/Services/PrintService.cs
+++ StockView.UI/View/Services/PrintService.cs
@@ -28,9 +28,14 @@ namespace StockView.UI.View.Services
 
         private Table CreateTable(DataTable data, DataTable summaryData, string title)
         {
-            var table = new Table();
+            var table = new Table()
+            {
+                BorderThickness = GlobalFormating.TableBorderThickness,
+                BorderBrush = GlobalFormating.TableBorderBrush,
+                FontFamily = new FontFamily("Calibri"),
+                CellSpacing = 0
+            };
 
-            // TODO: global formatting
             for (var x = 0; x < data.Columns.Count; x++)
             {
                 table.Columns.Add(new TableColumn());
@@ -50,36 +55,38 @@ namespace StockView.UI.View.Services
             return table;
         }
 
-        private static TableRow CreateTitleRow(DataTable data, string title)
+        private TableRow CreateTitleRow(DataTable data, string title)
         {
             // Title Row
             var row = new TableRow
             {
-                // TODO: formatting
+                Background = GlobalFormating.TableTitleBackground,
+                FontSize = GlobalFormating.TableTitleFontSize,
+                FontWeight = GlobalFormating.TableTitleFontWeight
             };
-            var titleCell = new TableCell(
-                new Paragraph(
-                    new Run(title)
-                )
-            );
+            var titleCell = new TableCell(new Paragraph(new Run(title)))
+            {
+                Padding = new Thickness(GlobalFormating.TableCellSpacing)
+            };
             titleCell.ColumnSpan = data.Columns.Count;
             row.Cells.Add(titleCell);
             return row;
         }
 
-        private static TableRow CreateHeaderRow(DataTable data)
+        private TableRow CreateHeaderRow(DataTable data)
         {
             TableRow row;
             // Column headers
             row = new TableRow
             {
-                // Formatting
+                FontSize = GlobalFormating.ColumnsHeaderRowFontSize,
+                FontWeight = GlobalFormating.ColumnsHeaderRowFontWeight
             };
             for (var i = 0; i < data.Columns.Count; i++)
             {
                 var column = data.Columns[i];
                 row.Cells.Add(
-                    new TableCell(
+                    CreateCell(
                         new Paragraph(
                             new Run(column.ColumnName)
                         )
@@ -94,7 +101,8 @@ namespace StockView.UI.View.Services
         {
             var row = new TableRow
             {
-                // Formatting
+                FontSize = GlobalFormating.DataRowsFontSize,
+                FontWeight = GlobalFormating.DataRowsFontWeight
             };
             foreach (var column in dataRow.Table.Columns.Cast<DataColumn>())
             {
@@ -107,7 +115,7 @@ namespace StockView.UI.View.Services
                 {
                     row.Cells.Add(CreateSnapshotCell(wrapper, prevRow?[column] as StockSnapshotWrapper));
                 } else if (obj is DBNull) {
-                    row.Cells.Add(new TableCell(
+                    row.Cells.Add(CreateCell(
                         new Paragraph(new Run("-"))
                         {
                             TextAlignment = TextAlignment.Center
@@ -121,7 +129,7 @@ namespace StockView.UI.View.Services
         private TableCell CreateDateCell(DateTime date)
         {
             var text = date.ToString("dd-MMM");
-            return new TableCell(
+            return CreateCell(
                 new Paragraph(
                     new Run(text)
                 )
@@ -145,7 +153,38 @@ namespace StockView.UI.View.Services
             {
                 paragraph.Inlines.Add(new Run("xd"));
             }
-            return new TableCell(paragraph);
+            return CreateCell(paragraph);
         }
+
+        private TableCell CreateCell(Block block)
+        {
+            return new TableCell(block)
+            {
+                BorderThickness = new Thickness(0.3),
+                Padding = new Thickness(GlobalFormating.TableCellSpacing),
+                BorderBrush = Brushes.LightGray
+            };
+        }
+    }
+
+    public class GlobalFormating
+    {
+        // -------------
+        public static double TableCellSpacing { get; set; } = 5;
+        public static Thickness TableBorderThickness { get; set; } = new Thickness() { Top = 1, Left = 1, Bottom = 1, Right = 1 };
+        public static Brush TableBorderBrush { get; set; } = Brushes.Black;
+        public static Brush TableTitleBackground { get; set; } = Brushes.Silver;
+        public static double TableTitleFontSize { get; set; } = 18;
+        public static FontWeight TableTitleFontWeight { get; set; } = FontWeights.Bold;
+        // -------------
+        public static Brush TableFooterBackground { get; set; } = Brushes.LightGray;
+        public static double TableFooterFontSize { get; set; } = 18;
+        public static FontWeight TableFooterFontWeight { get; set; } = FontWeights.Normal;
+        // -------------
+        public static double ColumnsHeaderRowFontSize { get; set; } = 14;
+        public static FontWeight ColumnsHeaderRowFontWeight { get; set; } = FontWeights.Bold;
+        // -------------
+        public static double DataRowsFontSize { get; set; } = 11;
+        public static FontWeight DataRowsFontWeight { get; set; } = FontWeights.Normal;
     }
 }
